f9b51aa86a8c1622016384dbee029c2b42a00df3
Tushar Kusnur <tkusnur@theaiinstitute.com>
2024-09-24 03:01:53 -0400

    Add custom site config file

---
+++ app.js
@@ -0,0 +1,37 @@
+// @ts-check
+
+const { defineConfig } = require("./.app/app-config");
+
+module.exports = defineConfig({
+  title: "Pedantic Semantics",
+  description: "The personal notes of John Doe",
+  lang: "en",
+  customProperties: {
+    properties: [],
+  },
+  theme: {
+    color: "sky",
+  },
+  editThisNote: {
+    url: "https://example.com/edit/{{file}}",
+  },
+  sidebar: {
+    links: [],
+    sections: [],
+  },
+  panel: {
+    tableOfContents: true,
+    tags: true,
+    customProperties: true,
+    incomingLinks: true,
+    outgoingLinks: true,
+    externalLinks: true,
+  },
+  wikilinks: {
+    autoLabel: "ref",
+    anchorLabel: "none",
+  },
+  tags: {
+    map: {},
+  },
+});
